afdfd33cdb44e3435d07a54df96a8ad5dd5c21d9
root <root@raspberrypi.(none)>
2013-07-08 22:34:52 +0000

    it didn't work before because of pdb and file perms.  I fixed it.

--- bin/airdrop-immunizer.py
+++ bin/airdrop-immunizer.py
@@ -9,15 +9,8 @@ import json
 import sys
 import os
 import re
+from get_mac import *
 import pdb
-import socket
-import fcntl, socket, struct, sys
-
-def get_mac(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
-    return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
-
 
 # Ripped this out of the wifiobjects from py, because it was simpler...bad crypt0s.
 class accessPoint:
@@ -79,15 +72,23 @@ immuneclient.bssid = mac
 filename = sys.argv[2]
 file_exists = os.path.exists(filename)
 # Open the filename and write changes
-with open(filename,'r') as conffile:
-    # Determine if the file exists and if it does, open it as a json object.
-    if file_exists:
-        data = json.load(conffile)
-    else:
-        data = {}
+# Determine if the file exists and if it does, open it as a json object.
+if file_exists:
+    conffile = open(filename,'r+')
+    data = json.load(conffile)
+else:
+    conffile = open(filename,'w')
+    data = {}
+
+conffile.close()
 
 data['ap'+mac] = immuneap.__dict__
 data['client'+mac] = immuneclient.__dict__
+try:
+    del data['client'+mac]['apObject']
+except:
+    pass
+    #i'm lazy, sue me.
 
 # Remove the Nones and replace with ""
 # Note: does it matter if it's "" vs null when parsed by the rule parser and compared on the match?!
@@ -99,3 +100,50 @@ for rule in data.keys():
 with open(filename,'w') as conffile:
     conffile.write(json.dumps(data))
     print str(sys.argv[1]) + " immunization written to config file at " + filename
+
+# I may have to make my own ap and client wifiobject stubs here in order to keep the peace.    
+#class accessPoint:
+#    """
+#    Access point object
+#    """
+#    def __init__(self, bssid):
+#        self.ruletype = "allow"
+#        self.attack = "0"
+#        self.type = "access_point"
+#        # set first time seen
+#        self.fts = time.time()      # first time object is seen
+#        self.lts = None             # last time object is seen, update on every acccess
+#        self.connectedClients = []  # list of connected clients
+#        self.essid = None           # broadcasted essid
+#        self.bssid = bssid          # bssid of ap
+#        self.hidden = False         # denote if essid is hidden
+#        self.encryption = None      # show encryption level
+#        self.auth = None            # show authentication settings
+#        self.cipher = None          # cipher, either CCMP, TKIP, wep 64/128
+#        self.channel = None         # ap's channel
+#        self.ssidList = []          # rolling list of seen ssid's for this ap
+#        self.oui = self.populateOUI() # lookup the object oui
+#
+#class client:
+#    """
+#    Client object
+#    """
+#    def __init__(self, mac):
+#        """
+#        mac = client mac address in hex   
+#        """
+#        self.ruletype = "allow"
+#        self.attack = "0"
+#        self.type = "client"
+#        self.fts = time.time()        # first time object is seen
+#        self.lts = None               # last time object is seen, update on every access 
+#        self.mac = mac                # client mac address
+#        self.probes = []              # list of probe requests client broadcast
+#        self.assoicated = False       # list if client is associated to an ap
+#        self.bssid = None             # Bssid of assoicated ap
+#        self.wired = False            # not a wired client by default
+#        self.lastBssid = None         # last connected bssid
+#        self.managedFrame = False     # have we seen a managment frame from this client?
+#        self.oui = self.populateOUI() # populate clients oui lookup#
+#        self.apObject = None          # stores reference link to ap# object when connected to bssid
+
